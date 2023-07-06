d03144582bf31a111ab10d4bbe63bfd708aa1d08
Justin Martin <jaming@protonmail.com>
2023-07-06 11:32:03 -0400

    use iters for zfs io on linux

--- src/app/data_harvester/disks/zfs_io_counters.rs
+++ src/app/data_harvester/disks/zfs_io_counters.rs
@@ -62,7 +62,6 @@ pub fn zfs_io_stats() -> anyhow::Result<Vec<anyhow::Result<IoCounters>>> {
 /// Returns zpool I/O stats. Pulls data from `/proc/spl/kstat/zfs/*/objset-*`.
 #[cfg(target_os = "linux")]
 pub fn zfs_io_stats() -> anyhow::Result<Vec<anyhow::Result<IoCounters>>> {
-    let mut results = vec![];
     if let Ok(zpools) = std::fs::read_dir("/proc/spl/kstat/zfs") {
         let zpools_vec: Vec<std::path::PathBuf> = zpools
             .filter_map(|e| {
@@ -76,68 +75,81 @@ pub fn zfs_io_stats() -> anyhow::Result<Vec<anyhow::Result<IoCounters>>> {
                 })
             })
             .collect();
-
-        zpools_vec.iter().for_each(|zpool| {
-            if let Ok(datasets) = std::fs::read_dir(zpool) {
-                let datasets_vec: Vec<std::path::PathBuf> = datasets
-                    .filter_map(|e| {
-                        e.ok().and_then(|d| {
-                            let p = d.path();
-                            if p.is_file() && p.to_str()?.contains("objset-") {
-                                Some(p)
+        let results = zpools_vec
+            .iter()
+            .filter_map(|zpool| {
+                // go through each pool
+                if let Ok(datasets) = std::fs::read_dir(zpool) {
+                    let datasets_vec: Vec<std::path::PathBuf> =
+                        datasets // go through dataset
+                            .filter_map(|e| {
+                                e.ok().and_then(|d| {
+                                    let p = d.path();
+                                    if p.is_file() && p.to_str()?.contains("objset-") {
+                                        Some(p)
+                                    } else {
+                                        None
+                                    }
+                                })
+                            })
+                            .collect();
+                    let io_counters: Vec<anyhow::Result<IoCounters>> = datasets_vec
+                        .iter()
+                        .filter_map(|ds| {
+                            // get io-counter from each dataset
+                            if let Ok(contents) = std::fs::read_to_string(ds) {
+                                let mut read = 0;
+                                let mut write = 0;
+                                let mut name = "";
+                                contents.lines().for_each(|line| {
+                                    if let Some((label, value)) = line.split_once(' ') {
+                                        match label {
+                                            "dataset_name" => {
+                                                if let Some((_type, val)) =
+                                                    value.trim_start().rsplit_once(' ')
+                                                {
+                                                    name = val;
+                                                }
+                                            }
+                                            "nwritten" => {
+                                                if let Some((_type, val)) =
+                                                    value.trim_start().rsplit_once(' ')
+                                                {
+                                                    if let Ok(number) = val.parse::<u64>() {
+                                                        write = number;
+                                                    }
+                                                }
+                                            }
+                                            "nread" => {
+                                                if let Some((_type, val)) =
+                                                    value.trim_start().rsplit_once(' ')
+                                                {
+                                                    if let Ok(number) = val.parse::<u64>() {
+                                                        read = number;
+                                                    }
+                                                }
+                                            }
+                                            _ => {}
+                                        }
+                                    }
+                                });
+                                let counter = IoCounters::new(name.to_owned(), read, write);
+                                //log::debug!("adding io counter for zfs {:?}", counter);
+                                Some(Ok(counter))
                             } else {
                                 None
                             }
                         })
-                    })
-                    .collect();
-                for dataset in datasets_vec {
-                    // TODO: [OPT] is this efficient?
-                    if let Ok(contents) = std::fs::read_to_string(dataset) {
-                        let mut nread = 0;
-                        let mut nwritten = 0;
-                        let mut ds_name = "";
-                        for line in contents.lines() {
-                            if let Some((label, value)) = line.split_once(' ') {
-                                match label {
-                                    "dataset_name" => {
-                                        if let Some((_type, val)) =
-                                            value.trim_start().rsplit_once(' ')
-                                        {
-                                            ds_name = val;
-                                        }
-                                    }
-                                    "nwritten" => {
-                                        if let Some((_type, val)) =
-                                            value.trim_start().rsplit_once(' ')
-                                        {
-                                            if let Ok(number) = val.parse::<u64>() {
-                                                nwritten = number;
-                                            }
-                                        }
-                                    }
-                                    "nread" => {
-                                        if let Some((_type, val)) =
-                                            value.trim_start().rsplit_once(' ')
-                                        {
-                                            if let Ok(number) = val.parse::<u64>() {
-                                                nread = number;
-                                            }
-                                        }
-                                    }
-                                    _ => continue,
-                                }
-                            }
-                        }
-                        if !ds_name.is_empty() {
-                            let counter = IoCounters::new(ds_name.to_owned(), nread, nwritten);
-                            //log::debug!("adding io counter for zfs {:?}", counter);
-                            results.push(Ok(counter));
-                        }
-                    }
+                        .collect();
+                    Some(io_counters)
+                } else {
+                    None
                 }
-            }
-        });
+            })
+            .flatten()
+            .collect(); // combine io-counters
+        Ok(results)
+    } else {
+        Err(anyhow::anyhow!("Unable to open zfs proc directory"))
     }
-    Ok(results)
 }
